6d44babf73ed264252f23b0a1c8986df50cf3b86
dkaznacheev <dmitry.kaznacheev@gmail.com>
2019-04-03 12:40:41 +0300

    Bot added

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+import os
+
+print(os.environ.get('SS_TOKEN'))
+
+bot = telebot.TeleBot(os.environ.get('SS_TOKEN'))
+
+
+@bot.message_handler(commands=['start'])
+def sayhi(message):
+    bot.reply_to(message, text="HELLO")
+
+
+if __name__ == '__main__':
+    bot.polling()
